04aaede1be671ef9b04c0484fa48a291bfae95c6
abbas usman <johnmccain2244@gmail.com>
2023-10-03 11:18:22 +0900

    feat: Frontend javascriptni qurish

--- app.js
+++ app.js
@@ -17,12 +17,27 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4 Routing code
-app.get("/create-item", (req, res) => {
-   // TODO: code with db here
+app.post("/create-item", (req, res) => {
+   console.log("user entered /create-item");
+   const new_reja =req.body.reja;
+   db.collection("plans").insertOne({ reja: new_reja }, (err, data) => { 
+      console.log(data.ops);
+      res.json(data.ops[0]);
+  });
 });
 
 app.get("/", function (req, res) {
-    res.render("reja");
+   console.log("user entered /");
+   db.collection("plans")
+   .find()
+   .toArray((err, data) => {
+      if (err) {
+         console.log(err);
+         res.end("something went wrong");
+      } else {
+         res.render("reja", { items: data });
+      }
+   });
  });
 
 module.exports = app;
