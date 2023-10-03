2ecee55979acde39be25c978ec99f575dd8c3b78
abbas usman <johnmccain2244@gmail.com>
2023-10-03 18:18:43 +0900

    feat: Reja loyhamizning yakuniniy bosqich

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 
 // MongoDB chaqirish
 const db = require("./server").db();
+const mongodb = require("mongodb");
 
 // 1 kirish code
 app.use(express.static("public"));
@@ -24,6 +25,22 @@ app.post("/create-item", (req, res) => {
       console.log(data.ops);
       res.json(data.ops[0]);
   });
+});
+
+app.post("/delete-item", (req, res) => {
+   const id = req.body.id;
+   db.collection("plans").deleteOne(
+      { _id: new mongodb.ObjectId(id) },
+      function (err, data) {
+         res.json({ state: "success" });
+      }
+   ); 
+});
+
+app.post("/edit-item", (req, res) => {
+   const data = req.body;
+   console.log(data);
+   res.end("done");
 });
 
 app.get("/", function (req, res) {
--- train.js
+++ train.js
@@ -1,35 +1,72 @@
-console.log("Jack Ma maslahatlari");
-const list = [
-    'yaxshi talaba boling', //0-20
-     'togri boshiq tanlang va koproq hato qiling', // 20-30
-     'oz ishingizni boshlang', // 30-40
-     'siz kuchli bolgan narsalarni qiling', //40-50
-     'yoshlarga investitsiya qiling', // 50-60
-     'endi dam oling, foydasi yoq'
- ];
+//                              Task - F
+//
+// const getReverse = function(str) {
+//     for(let i = 0; i <= str.length; i++) {
+//         for(let a = i+1; a <= str.length; a++) {
+//             if(str[a] === str[i]) {
+//                 return true;
+//             }
+//         }
+//     }
+//     return false;
+// }
+//
+// const input = "hello";
+// const result = getReverse(input);
+// console.log(result)
+//
 
 
-function maslahatbering (a, callback) {
-    if ( typeof a !== "number") callback("insert a number", null);
-    else if (a <= 20) callback(null, list[0]);
-    else if (a > 20 && a <= 30) callback(null, list[1]);
-    else if (a > 30 && a <= 40) callback(null, list[2]);
-    else if (a > 40 && a <= 50) callback(null, list[3]);
-    else if (a > 50 && a <= 60) callback(null, list[4]);
-    else {
-        setTimeout(function () {
-            callback(null, list[5]);
-        }, 5000);
-    }
-}
 
 
-console.log("passed here 0")
-maslahatbering(25, (err, data) => {
-    if(err) console.log("ERROR:", err)
-    else {
-        console.log("javob:", data);
-    }
-})
+//                               Task - E
 
- console.log("passed here 1");
+// function reverse (str) {
+//     if (str === "")
+//         return "";
+//     else
+//         return reverse(str.substring(1)) + str.charAt(0);
+// }
+// let str = "hello";
+// const output = reverse(str);
+// console.log(output);
+//
+
+
+
+
+// console.log("Jack Ma maslahatlari");
+// const list = [
+//     'yaxshi talaba boling', //0-20
+//      'togri boshiq tanlang va koproq hato qiling', // 20-30
+//      'oz ishingizni boshlang', // 30-40
+//      'siz kuchli bolgan narsalarni qiling', //40-50
+//      'yoshlarga investitsiya qiling', // 50-60
+//      'endi dam oling, foydasi yoq'
+//  ];
+
+
+// function maslahatbering (a, callback) {
+//     if ( typeof a !== "number") callback("insert a number", null);
+//     else if (a <= 20) callback(null, list[0]);
+//     else if (a > 20 && a <= 30) callback(null, list[1]);
+//     else if (a > 30 && a <= 40) callback(null, list[2]);
+//     else if (a > 40 && a <= 50) callback(null, list[3]);
+//     else if (a > 50 && a <= 60) callback(null, list[4]);
+//     else {
+//         setTimeout(function () {
+//             callback(null, list[5]);
+//         }, 5000);
+//     }
+// }
+
+
+// console.log("passed here 0")
+// maslahatbering(25, (err, data) => {
+//     if(err) console.log("ERROR:", err)
+//     else {
+//         console.log("javob:", data);
+//     }
+// })
+
+//  console.log("passed here 1");
