42cf51f2074d2f12df00b2da95fa9b4630e77fe8
abbas usman <johnmccain2244@gmail.com>
2023-10-12 18:28:14 +0900

    fix: Task-I bajardim

--- train.js
+++ train.js
@@ -1,3 +1,16 @@
+function getCompute(arr) {
+    if (arr.lenght === 0) {
+        return arr;
+    }
+    const firstItem = arr[0];
+    arr.shift(); // 1 chi  qiymatni uchirmoqda.
+    arr.push(firstItem); // (0) chi index qiymatni oxiridan qushsin.
+    return arr;
+}
+const inputArray = ['h','e','l','l','o'];
+const result = getCompute(inputArray);
+console.log(result);
+
 //                              Task - F
 //
 // const getReverse = function(str) {
